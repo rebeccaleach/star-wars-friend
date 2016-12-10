f9629395b250a537d586a787f5f8c27eba429a42
Rebecca Leach <flytrapcat@gmail.com>
2016-12-10 12:38:06 -0600

    basic frontend added

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,11 +1,18 @@
 console.log('api-routes.js is connected.');
 
+var path = require('path');
+
 module.exports = function(app) {
 
 	app.get('/', function(req, res) {
 		// res.send is working, but res.sendFile is not
-		res.send('Is this thing on?????');
-		// res.sendFile(path.join(__dirname, 'index.html'));
+		// - apparently it wasn't working because i didn't put require path in api-routes, even though it was server.js. why?
+		// res.send('Is this thing on?????');
+		res.sendFile(path.join(__dirname, '../public/index.html'));
+	});
+
+	app.get('/questions', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/questions.html'));
 	});
 
 };
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //// dependencies
 
 // so require is actually a function? where is the function defined? is it a node thing?
+// - yes, require is a node module. modules are different for browser, node, etc.
 var express = require('express');
 var bodyParser = require('body-parser');
+// why is path required here if we don't use it until api-routes.js? in the hot restaurant exercise, path was required in the routes files as well as the server file. why?
 var path = require('path'); // not sure if this is necessary. is it a core module?
 
 
@@ -16,15 +18,6 @@ var PORT = 3000;
 
 
 
-// why do we need the ./ here?
-// why do we need (app)? is it talking to api-routes.js to tell it what "app" means? is this calling two functions at once?
-// why did the hot restaurant activity not use the "var apiRoutes =" part of this require (below)?
-// why is the server saying .get is undefined?
-// - you have to call this function AFTER you define app (i.e., var app = express(); ). the below version defines apiRoutes and calles the express function (i think). alternately, you just just define it (var apiRoutes = require('...'); ) and then call it later ( apiRoutes(app); )
-var apiRoutes = require('./app/routing/api-routes.js')(app);
-
-
-
 // data parsing - what is this? what does all this code mean? what exactly is it doing?
 // - https://www.npmjs.com/package/body-parser
 // - what is middleware?
@@ -33,7 +26,19 @@ var apiRoutes = require('./app/routing/api-routes.js')(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+// app.use(bodyParser.json({type: 'application/vnd.api+json'})); // probably don't need this
+app.use(express.static('app/public'));
+
+
+
+
+// why do we need the ./ here?
+// why do we need (app)? is it talking to api-routes.js to tell it what "app" means? is this calling two functions at once?
+// why did the hot restaurant activity not use the "var apiRoutes =" part of this require (below)?
+// why is the server saying .get is undefined?
+// - you have to call this function AFTER you define app (i.e., var app = express(); ). the below version defines apiRoutes and calles the express function (i think). alternately, you just just define it (var apiRoutes = require('...'); ) and then call it later ( apiRoutes(app); )
+var apiRoutes = require('./app/routing/api-routes.js')(app);
+
 
 
 
